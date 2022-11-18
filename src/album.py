2424b2d59207085cb5c9b84a61b89fb08611d0c7
EdisonYCA <edisonyca127@gmail.com>
2022-11-17 22:55:22 -0500

    Create Album class to represent an artist's album

---
+++ src/album.py
@@ -0,0 +1,12 @@
+class Album:
+    def __init__(self, name, songs):
+        self.name = name
+        self.songs = songs
+
+    def get_album_name(self):
+        """Return name of album"""
+        return self.name
+
+    def get_album_songs(self):
+        """Return songs in album"""
+        return self.songs
